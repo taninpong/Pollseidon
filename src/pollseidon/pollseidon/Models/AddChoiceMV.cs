adeef3292aca1a9fc3942d85913a231db9c1bcc3
Anawat <anawat@perfenterprise.com>
2018-08-18 13:22:49 +0700

    Add VM
    Signed-off-by: Anawat <anawat@perfenterprise.com>

---
+++ src/pollseidon/pollseidon/Models/AddChoiceMV.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pollseidon.Models
+{
+    public class AddChoiceVM
+    {
+        public string TopicId;
+        public string ChoiceName; 
+    }
+}
