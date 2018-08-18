4625f89b2474bfb06f90436c4547d5bb6df07404
wanchana <wanchana@perfenterprise.com>
2018-08-18 13:26:16 +0700

    add Test Project And new modelVM

---
+++ src/pollseidon/pollseidon.models/ViewModels/ChoiceVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pollseidon.models.ViewModels
+{
+    public class ChoiceVM
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string CraeteBy { get; set; }
+        public DateTime CraeteDate { get; set; }
+        public double Rating { get; set; }
+    }
+}
---
+++ src/pollseidon/pollseidon.models/ViewModels/TopicVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pollseidon.models.ViewModels
+{
+    public class TopicVM
+    {
+        public string id { get; set; }
+        public string TopicName { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public IEnumerable<ChoiceVM> ChoiceList { get; set; }
+        public IEnumerable<VoteVM> VoteList { get; set; }
+        public int VoteCount { get; set; }
+    }
+}
